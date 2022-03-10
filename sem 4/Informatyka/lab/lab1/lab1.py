93faf185ca7147ee535fc528833129c1e25c1e2c
Jakub Kruszyk <k.kruszyk7@gmail.com>
2022-03-10 11:24:50 +0100

    lab1 refactor

--- sem 4/Informatyka/lab/lab1/lab1.py
+++ sem 4/Informatyka/lab/lab1/lab1.py
@@ -1,17 +1,15 @@
 text = input("Podaj liczby rozdzielone spacją:\n")
 nums = [int(x) for x in text.split(" ")]
-file = open('text.txt', 'w')
-# for num in nums:
-#     if num % 2:
-#         num_str = str(num % 1000)
-#     else:
-#         num_str = str(num % 100)
-#     print(num_str)
-#     file.write(num_str + '\n')
+with open('text.txt', 'w') as file:
+    # for num in nums:
+    #     if num % 2:
+    #         num_str = str(num % 1000)
+    #     else:
+    #         num_str = str(num % 100)
+    #     print(num_str)
+    #     file.write(num_str + '\n')
 
-nums = [str(num % 100) if num % 2 else str(num % 1000) for num in nums]
-for num in nums:
-    print(num)
-    file.write(num + '\n')
-
-file.close()
+    nums = [str(num % 100) if num % 2 else str(num % 1000) for num in nums]
+    for num in nums:
+        print(num)
+        file.write(num + '\n')
