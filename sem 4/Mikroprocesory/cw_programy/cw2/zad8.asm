630c95225019033d88ff8a8619cccb72d05364df
Jakub Kruszyk <k.kruszyk7@gmail.com>
2022-03-18 12:29:53 +0100

    Poprawka do uC cw2 zad8

--- sem 4/Mikroprocesory/cw_programy/cw2/zad8.asm
+++ sem 4/Mikroprocesory/cw_programy/cw2/zad8.asm
@@ -2,14 +2,14 @@
 	ORG	100H
 START:
 	SETB RS0
-	SETB RS1
-	MOV R0, #20H
-	MOV ACC, #7
+	SETB RS1 ; ustawienie banku 3
+	MOV R0, #1FH ; adres rejestru R7
+	MOV ACC, #7 ; licznik do pêtli
 LOOP:
-	DEC R0
-	MOV @R0, ACC
-	DJNZ ACC, LOOP
-	MOV R0, #0
+	MOV @R0, ACC ; wpisanie licznika do rejestru (adresowanie poœrednie)
+	DEC R0 ; zmniejszenie adresu o 1
+	DJNZ ACC, LOOP ; zmniejszenie licznika o 1, jeœli licznik =/= 0 skocz do LOOP
+	MOV R0, #0 ; ustaw R0 na 0
 STOP:			;nie wykonuj innych dzia³añ
 	LJMP	STOP	;- pozostañ w pêtli STOP
 	NOP
