cd9fa9772a6a64d05591bae68de9299c3545ff1e
Jakub Kruszyk <k.kruszyk7@gmail.com>
2022-03-20 20:31:07 +0100

    Poprawki IT lab2 zad2
    Dodano funkcje z zadania domowego

--- sem 4/Informatyka/lab/lab2/zad2.py
+++ sem 4/Informatyka/lab/lab2/zad2.py
@@ -21,20 +21,64 @@ class School:
         num = input("Podaj liczbę nauczycieli: ")
         num = int(num) if num.isnumeric() else 0
         for i in range(num):
-            self.teachers.append(Teacher())
+            self.add_teacher()
 
         num = input("Podaj liczbę kursów: ")
         num = int(num) if num.isnumeric() else 0
         for i in range(num):
-            self.courses.append(Course(self.teachers))
+            self.add_course()
 
         num = input("Podaj liczbę studentów: ")
         num = int(num) if num.isnumeric() else 0
         for i in range(num):
-            self.students.append(Student(self.courses))
+            self.add_student()
 
     def __repr__(self):
         return f"{self.name} school. Address: {self.address}"
+
+    def print_students(self):
+        students_sorted = sorted(self.students, key=lambda s: s.email)
+        for student in students_sorted:
+            print(student)
+    
+    def print_teachers(self):
+        teachers_sorted = sorted(self.teachers, key=lambda t: t.email)
+        for teacher in teachers_sorted:
+            print(teacher)
+
+    def print_courses(self, sem):
+        courses_filtered = [c for c in self.courses if c.semester == sem]
+        if courses_filtered:
+            for course in courses_filtered:
+                print(course)
+        else:
+            print("Nie znaleziono kursów dla danego semestru")
+
+    def print_students_age(self):
+        pass
+
+    def add_student(self):
+        self.students.append(Student(self.courses))
+
+    def add_course(self):
+        self.courses.append(Course(self.teachers))
+
+    def add_teacher(self):
+        self.teachers.append(Teacher())
+
+    def average(self):
+        average_list = {c.name: [] for c in self.courses}
+        students_results = [student.average() for student in self.students]
+        for result in students_results:
+            for course in result:
+                average_list[course].append(result[course])
+
+        for course in average_list:
+            if average_list[course]:
+                average_list[course] = sum(average_list[course])/len(average_list[course])
+            else:
+                average_list[course] = 0
+        return average_list
 
 
 class Teacher:
@@ -51,13 +95,13 @@ class Teacher:
 class Course:
     def __init__(self, teachers):
         self.name = input("Podaj nazwę: ")
-        self.semester = input("Podaj semestr: ")
+        self.semester = int(input("Podaj semestr: "))
         self.students = []
         pesel = input("Podaj pesel nauczyciela: ")
         teacher = [t for t in teachers if t.pesel == pesel][0]
         if teacher:
-            teacher[0].courses.append(self)
-            self.teacher = teacher[0]
+            teacher.courses.append(self)
+            self.teacher = teacher
         else:
             print("Nie znaleziono nauczyciela z danym numerem pesel")
 
@@ -71,21 +115,53 @@ class Student:
         self.email = input("Podaj email: ")
         self.semester = input("Podaj semestr: ")
         self.courses = []
+        self.grades = {}
         num = input("Podaj liczbę kursów: ")
         num = int(num) if num.isnumeric() else 0
         if num > len(courses):
             num = len(courses)
         for i in range(num):
             name = input("Podaj nazwę kursu: ")
-            course = [c for c in courses if c.name == name]
+            course = [c for c in courses if c.name == name][0]
             if course:
-                self.courses.append(course[0])
-                course[0].students.append(self)
+                self.courses.append(course)
+                course.students.append(self)
+                self.grades[course.name] = []
             else:
                 print("Zła nazwa kursu")
 
     def __repr__(self):
         return f"Email: {self.email}, semester: {self.semester}"
 
+    def add_grade(self, course, grade):
+        grade_list = self.grades.get(course)
+        if grade_list is None:
+            print("Uczeń nie jest zapisany do tego kursu")
+        else:
+            grade_list.append(grade)
+
+    def average(self, course=None):
+        if course is None:
+            average_list = {c: [] for c in self.grades}
+        else:
+            average_list = {course: []}
+        for course in average_list:
+            if self.grades[course]:
+                average_list[course] = sum(self.grades[course])/len(self.grades[course])
+            else:
+                average_list[course] = 0
+        return average_list
+
 
 nowa = School()
+nowa.print_students()
+nowa.print_teachers()
+nowa.print_courses(1)
+nowa.print_students_age()
+
+nowa.students[0].add_grade("matematyka", 5.0)
+nowa.students[0].add_grade("matematyka", 4.5)
+nowa.students[0].add_grade("inf", 4.0)
+nowa.students[0].add_grade("inf", 3.0)
+print(nowa.students[0].average())
+print(nowa.average())
