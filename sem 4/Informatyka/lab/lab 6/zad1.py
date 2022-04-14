ba6668639a064af18e8dcc277f519ee0899231b0
Jakub Kruszyk <k.kruszyk7@gmail.com>
2022-04-14 13:54:37 +0200

    Added IT lab 6

---
+++ sem 4/Informatyka/lab/lab 6/zad1.py
@@ -0,0 +1,7 @@
+# zrobić wykres np temperatury od daty
+# matplotlib
+# dane api --> dowolne, wybrac sb
+# dzialanie:
+# 1. odpalenie programu: pobieranie danych(miejsce - miasto/koordynaty,
+#                                           daty - opcjonalnie, request, odpowiedz zapis do pliku)
+# zwrócić uwagęna duplikaty, chronologia, całość w pętli 1-kontunuuj, 2-wizualizacja, 3-wyjscie
