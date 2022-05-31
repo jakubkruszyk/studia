446fd27aa003c0b87f0a89ecec268c6bf82347e7
Jakub Kruszyk <k.kruszyk7@gmail.com>
2022-05-31 17:44:00 +0200

    Updated IT lab 8

--- sem 4/Informatyka/lab/lab 8/photo.py
+++ sem 4/Informatyka/lab/lab 8/photo.py
@@ -3,38 +3,43 @@ import cv2
 import sewar
 from filters import *
 
-REF_DIR = "./faces"
+if __name__ == '__main__':
 
-check_path = input("Podaj ścieżkę do pliku/ folderu:\n")
-use_own = True if input("Czy użyć własnej metody (y/n): ") == "y" else False
-use_ssim = True if input("Czy użyć ssim (trwa bardzo długo) (y/n): ") == "y" else False
+    REF_DIR = "./faces"
 
-if os.path.isdir(check_path):
-    file_list = [os.path.join(check_path, path) for path in os.listdir(check_path)
-                 if os.path.isfile(os.path.join(check_path, path))]
-else:
-    file_list = [check_path]
+    check_path = input("Podaj ścieżkę do pliku/ folderu:\n")
+    use_own = True if input("Czy użyć własnej metody (y/n): ") == "y" else False
+    use_ssim = True if input("Czy użyć ssim (trwa bardzo długo) (y/n): ") == "y" else False
 
-for i in file_list:
-    rmse_vec = []
-    ssim_vec = []
-    own_vec = []
-    img_to_check = cv2.imread(i)
-
-    for file_name in os.listdir(REF_DIR):
-        ref_image_path = os.path.join(REF_DIR, file_name)
-        ref_image = cv2.imread(ref_image_path)
-        rmse_vec.append(sewar.full_ref.rmse(img_to_check, ref_image))
-        if use_own:
-            own_vec.append(own_method(img_to_check, ref_image))
-        if use_ssim:
-            ssim_vec.append(sewar.full_ref.ssim(img_to_check, ref_image)[0])
-
-    print(avg(rmse_vec))
-    print(avg(own_vec))
-    print(avg(ssim_vec))
-
-    if (avg(rmse_vec) < 100) and (avg(own_vec) < 100 or not use_own) and (avg(ssim_vec) > 0.8 or not use_ssim):
-        print("Na zdjęciu", i, "jest twarz")
+    if os.path.isdir(check_path):
+        file_list = [os.path.join(check_path, path) for path in os.listdir(check_path)
+                     if os.path.isfile(os.path.join(check_path, path))]
+    elif os.path.isfile(check_path):
+        file_list = [check_path]
     else:
-        print("Na zdjęciu", i, "nie ma twarzy")
+        print("zła ścieżka")
+        exit()
+
+    for i in file_list:
+        rmse_vec = []
+        ssim_vec = []
+        own_vec = []
+        img_to_check = cv2.imread(i)
+
+        for file_name in os.listdir(REF_DIR):
+            ref_image_path = os.path.join(REF_DIR, file_name)
+            ref_image = cv2.imread(ref_image_path)
+            rmse_vec.append(sewar.full_ref.rmse(img_to_check, ref_image))
+            if use_own:
+                own_vec.append(own_method(img_to_check, ref_image))
+            if use_ssim:
+                ssim_vec.append(sewar.full_ref.ssim(img_to_check, ref_image)[0])
+
+        print(avg(rmse_vec))
+        print(avg(own_vec))
+        print(avg(ssim_vec))
+
+        if (avg(rmse_vec) < 100) and (avg(own_vec) < 100 or not use_own) and (avg(ssim_vec) > 0.8 or not use_ssim):
+            print("Na zdjęciu", i, "jest twarz")
+        else:
+            print("Na zdjęciu", i, "nie ma twarzy")
